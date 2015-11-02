2d8bebd5826a73acd50217ab27d836282a5b99da
Noa Gudjonsdottir <noagudjons@gmail.com>
2015-11-02 16:50:50 +0000

    hasWon boolean

--- src/main/java/is/ru/ticTacToeGame/ticTacToe.java
+++ src/main/java/is/ru/ticTacToeGame/ticTacToe.java
@@ -25,6 +25,27 @@ public class TicTacToe{
 				}
 		}
 	}
+	
+	public boolean hasWon(){
+		//Check if player has won
+		if 		(board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") return true;
+		else if (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") return true;
+    	else if (board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") return true;
+    	else if (board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") return true;
+    	else if (board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") return true;
+    	else if (board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") return true;
+    	else if (board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") return true;
+    	else if (board[2][0] == "X" && board[1][1] == "X" && board[0][2] == "X") return true;
+    	else if (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O") return true;
+    	else if (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O") return true;
+    	else if (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O") return true;
+    	else if (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O") return true;
+    	else if (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O") return true;
+    	else if (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O") return true;
+    	else if (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O") return true;
+    	else if (board[2][0] == "O" && board[1][1] == "O" && board[0][2] == "O") return true;
+    	else return false;
+	}
 
 }
 		
