4ccb5511926a02e75348b130a644b2acb0996117
sigurjon <sigurjonb11@ru.is>
2015-11-02 16:03:23 +0000

    game test add

---
+++ test/java/is/ru/ticTacToeGame/gameTest.java
@@ -0,0 +1,28 @@
+package is.ru.ticTacToeGame;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import java.util.Scanner;
+
+
+import org.junit.Test;
+
+public class GameTest{
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("is.ru.ticTacToeGame.GameTest");
+	}
+
+	@Test
+	public void testConstructor(){
+		Game game1 = new Game();
+		assertTrue(game1.whosTurn);
+	}
+
+	@Test
+	public void testPlayGame(){
+		
+
+
+}
+
