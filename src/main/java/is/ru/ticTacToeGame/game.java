6cc965712e08d6e12c9ed7f0d301fd73c18c8ec7
robertingi <robertingi@gmail.com>
2015-11-02 16:30:19 +0000

    added printWins()

--- src/main/java/is/ru/ticTacToeGame/game.java
+++ src/main/java/is/ru/ticTacToeGame/game.java
@@ -3,37 +3,40 @@ package is.ru.ticTacToeGame;
 import java.util.Scanner;
 
 public class Game {
-	public Player Player1;
-	public Player Player2;
-	public TicTacToe theGame;
-	boolean whosTurn; //True for Player1, false for Player2.
-	
-	Game(){
-		Player1 = new Player("X");
-		Player2 = new Player("O");
-		theGame = new TicTacToe();
-		whosTurn=true;
-	}
+    public Player Player1;
+    public Player Player2;
+    public TicTacToe theGame;
+    boolean whosTurn; //True for Player1, false for Player2.
+    
+    Game(){
+        Player1 = new Player("X");
+        Player2 = new Player("O");
+        theGame = new TicTacToe();
+        whosTurn=true;
+    }
 
-	private boolean inputOK(int number){
-		
-		return true;
-	}
+    private boolean inputOK(int number){
+        if(number>9 || number<1){
+            return false;
+        }
+        return true;
+    }
 
-	public void printWins(){
-		
-	}
+    public void printWins(){
+        System.out.println("Player 1 points: "+ Player1.wins);
+        System.out.println("Player 2 points: "+ Player2.wins);
+    }
 
-	public void playGame(){
-		
-		
-	}
+    public void playGame(){
+        
+        
+    }
 
 
 
-	//mock function to test playGame!
-	public void MockPlayGame(Scanner in){
-		
-		}
-	}
+    //mock function to test playGame!
+    public void MockPlayGame(Scanner in){
+        
+        }
+    }
 
