aa691ad5064ad835d78d2b72c2c5302788d31aec
robertingi <robertingi@gmail.com>
2015-11-02 18:51:38 +0000

    blabla

--- src/main/java/is/ru/ticTacToeGame/game.java
+++ src/main/java/is/ru/ticTacToeGame/game.java
@@ -73,45 +73,51 @@ public class Game {
     }
 
 
-
     //mock function to test playGame!
     public void MockPlayGame(Scanner in){
-            while(!theGame.hasWon() && !theGame.isFull()){
-            int number;
-            number = in.nextInt();
-            
-            if(inputOK(number) && !theGame.isSeatTaken(number)){
-                
-                if(theGame.whosTurn){
-                    theGame.move(number,Player1.mark);
-                }
-                else{
-                    theGame.move(number,Player2.mark);
-                }
 
-                if(theGame.hasWon() && theGame.whosTurn){
-                    theGame.print();
-                    System.out.println("Player 2 wins");
-                    theGame.clearAll();
-                    Player2.wins++;
-                    theGame.whosTurn=true;
-                }
-                else if(theGame.hasWon() && !theGame.whosTurn){
-                    theGame.print();
-                    System.out.println("Player 1 wins");
-                    theGame.clearAll();
-                    Player1.wins++;
-                    theGame.whosTurn=true;
+            while(!theGame.hasWon() && !theGame.isFull() && in.hasNext())
+            {
+                int number;
+                number = in.nextInt();
+                
+                if(inputOK(number) && !theGame.isSeatTaken(number))
+                {
                     
+                    if(theGame.whosTurn){
+                        theGame.move(number, Player1.mark);
+                    }
+                    else{
+                        theGame.move(number, Player2.mark);
+                    }
+
+                    if(theGame.hasWon() && theGame.whosTurn){
+                        System.out.println("Player 2 wins!");
+
+                        theGame.clearAll();
+                        Player2.wins++;
+                        theGame.whosTurn = true;
+                        printWins();
+                    }
+                    else if(theGame.hasWon() && !theGame.whosTurn){
+                        System.out.println("Player 1 wins!");
+
+                        theGame.clearAll();
+                        Player1.wins++;
+                        theGame.whosTurn = true;
+                        printWins();
+                        
+                    }
+                    else if(theGame.isFull()){
+                        System.out.println("It's a tie.");
+                        theGame.whosTurn = true;
+                        theGame.clearAll();
+                        printWins();
+                        
+                    }
                 }
-                else if(theGame.isFull()){
-                    theGame.print();
-                    System.out.println("It's a tie");
-                    theGame.whosTurn=true;
-                    theGame.clearAll();
-                    
-                }
+                theGame.print();
             }
-        } 
+        }
     }
 
