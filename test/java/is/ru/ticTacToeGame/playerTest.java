f771b6f2ea39f3a0bcf5ba63c3794fcf773ae0f8
sigurjon <sigurjonb11@ru.is>
2015-11-02 16:10:23 +0000

    player test add

---
+++ test/java/is/ru/ticTacToeGame/playerTest.java
@@ -0,0 +1,15 @@
+package is.ru.ticTacToeGame;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class playerTest{
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("is.ru.ticTacToeGame.playerTest");
+	}
+	@Test
+	public void testPlayer(){
+		Player player = new Player("X");
+		assertEquals("X",player.mark);
+		
+	}		
+	
+}
