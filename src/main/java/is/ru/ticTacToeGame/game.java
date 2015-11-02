b44d8adda4aade9feafc956322c4a35c2b68d122
robertingi <robertingi@gmail.com>
2015-11-02 16:41:28 +0000

    added functionality

--- src/main/java/is/ru/ticTacToeGame/game.java
+++ src/main/java/is/ru/ticTacToeGame/game.java
@@ -28,8 +28,48 @@ public class Game {
     }
 
     public void playGame(){
-        
-        
+        while(!theGame.hasWon() && !theGame.isFull()){
+            int number;
+            Scanner in = new Scanner(System.in);
+            number = in.nextInt();
+            
+            if(inputOK(number) && !theGame.isSeatTaken(number)){
+                
+                if(theGame.whosTurn){
+                    theGame.move(number,Player1.mark);
+                }
+                else{
+                    theGame.move(number,Player2.mark);
+                }
+
+                if(theGame.hasWon() && theGame.whosTurn){
+                    theGame.print();
+                    System.out.println("Player 2 wins");
+                    theGame.clearAll();
+                    Player2.wins++;
+                    theGame.whosTurn=true;
+                    printWins();
+                }
+                else if(theGame.hasWon() && !theGame.whosTurn){
+                    theGame.print();
+                    System.out.println("Player 1 wins");
+                    theGame.clearAll();
+                    Player1.wins++;
+                    theGame.whosTurn=true;
+                    printWins();
+                    
+                }
+                else if(theGame.isFull()){
+                    theGame.print();
+                    System.out.println("It's a tie");
+                    theGame.whosTurn=true;
+                    theGame.clearAll();
+                    printWins();
+                    
+                }
+            }
+            theGame.print();
+        } 
     }
 
 
