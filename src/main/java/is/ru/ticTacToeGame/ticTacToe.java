93caecb29994816c2b82b0c294f46faa7df2789b
Noa Gudjonsdottir <noagudjons@gmail.com>
2015-11-02 18:04:16 +0000

    print ticTacToe

--- src/main/java/is/ru/ticTacToeGame/ticTacToe.java
+++ src/main/java/is/ru/ticTacToeGame/ticTacToe.java
@@ -59,6 +59,14 @@ public class TicTacToe{
 			}
 			return true;
 	}
+	
+	public void print(){
+		System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
+		System.out.println("-----");
+		System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
+		System.out.println("-----");
+		System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+	}
 
 }
 		
