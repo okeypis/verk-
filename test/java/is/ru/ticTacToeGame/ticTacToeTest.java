ae9f5a160189f0f0beb6089c93b4229ee78b1e9a
sigurjon <sigurjonb11@ru.is>
2015-11-02 16:13:52 +0000

    tictactoe test add

---
+++ test/java/is/ru/ticTacToeGame/ticTacToeTest.java
@@ -0,0 +1,49 @@
+package is.ru.ticTacToeGame;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+
+import org.junit.Test;
+
+public class TicTacToeTest{
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("is.ru.ticTacToeGame.TicTacToeTest");
+	}
+
+	@Test
+	public void testConstructor(){
+
+	}
+
+	@Test
+	public void testMove(){
+		
+	}
+
+	@Test
+	public void testisFull(){
+		
+	}
+
+	@Test
+	public void testHasWon(){
+		
+
+
+
+	} 
+
+	@Test
+	public void testClearAll(){
+	
+	}
+
+	@Test
+	public void testIsSeatTaken(){
+		
+	}	
+
+	
+}
