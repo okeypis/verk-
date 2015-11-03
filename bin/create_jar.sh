4e5859bd4eb9a8f7ef8b11ca7c4a6fca669deae8
robertingi <robertingi@gmail.com>
2015-11-03 17:42:37 +0000

    did stuff

--- bin/create_jar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-jar cmf MANIFEST.txt TicTacToe.jar classes/is/ru/ticTacToeGame/Main.class classes/is/ru/ticTacToeGame/Game.class classes/is/ru/ticTacToeGame/Player.class classes/is/ru/ticTacToeGame/TicTacToe.class
